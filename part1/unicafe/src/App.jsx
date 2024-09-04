471a7ef88f8337693d180f2bc0b2682a3c9d3863
Yannick Hagedorn <176614986+y-hagedorn@users.noreply.github.com>
2024-09-04 23:10:30 +0200

    Exercise 1.7 unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 const Display = (props) => {
   return (
-    <div>{props.feedback} {props.counter}</div>
+    <div>{props.category} {props.value} {props.unit}</div>
   )
 }
 
@@ -12,6 +12,10 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const total = good + neutral + bad
+  const average = (total === 0) ? 0 : (good * 1 + neutral * 0 + bad * (-1)) / total
+  const positive = (total === 0) ? 0 : (good / total) * 100
+
   return (
     <div>
       <h1>Give feedback</h1>
@@ -19,9 +23,12 @@ const App = () => {
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
       <h1>Statistics</h1>
-      <Display feedback="good" counter={good}></Display>
-      <Display feedback="neutral" counter={neutral}></Display>
-      <Display feedback="bad" counter={bad}></Display>
+      <Display category="good" value={good}></Display>
+      <Display category="neutral" value={neutral}></Display>
+      <Display category="bad" value={bad}></Display>
+      <Display category="all" value={total}></Display>
+      <Display category="average" value={average}></Display>
+      <Display category="positive" value={positive} unit="%"></Display>
     </div>
   )
 }
