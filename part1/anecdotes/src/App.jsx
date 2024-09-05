292ef5f427ba352dd9b91dfc23f8a85de20fa388
Yannick Hagedorn <176614986+y-hagedorn@users.noreply.github.com>
2024-09-05 18:26:14 +0200

    Exercise 1.14 anecdotes

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,5 +1,43 @@
 import { useState } from 'react'
 
+const Anecdote = (props) => {
+
+  return (
+    <>
+      <h1>Anecdote of the day</h1>
+      <div>{props.anecdotes[props.selected]}</div>
+      <p>has {props.votes[props.selected]} votes</p>
+
+    </> 
+  )
+}
+
+const Favorite = (props) => {
+  const mostVotes = Math.max(...props.votes)
+  const mostVotedAnecdote = props.votes.indexOf(mostVotes)
+
+  console.log('Most votes...', mostVotes)
+  console.log('for anecdote...', mostVotedAnecdote)
+
+  if (mostVotes === 0) {
+    return (
+      <>
+        <h1>Anecdote with most votes</h1>
+        <p>No votes at the moment</p>
+      </>
+    )
+  }
+
+  return (
+    <>
+      <h1>Anecdote with most votes</h1>
+      <div>{props.anecdotes[mostVotedAnecdote]}</div>
+      <p>has {mostVotes} votes</p>
+    </>
+  )
+
+}
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -11,12 +49,9 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
-   
+
   const [selected, setSelected] = useState(0)
   const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
-
-  console.log('votes...', votes)
-  console.log('random number...', selected)
 
   const voteForAnecdote = () => {
     const copy = [...votes]
@@ -24,12 +59,15 @@ const App = () => {
     setVotes(copy)
   }
 
+  console.log('votes...', votes)
+  console.log('random number...', selected)
+
   return (
     <div>
-      <div>{anecdotes[selected]}</div>
-      <p>has {votes[selected]} votes</p>
+      <Anecdote votes={votes} anecdotes={anecdotes} selected={selected}/>
       <button onClick={voteForAnecdote}>vote</button>
       <button onClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))}>next anecdote</button>
+      <Favorite votes={votes} anecdotes={anecdotes}/>
     </div>
   )
 }
