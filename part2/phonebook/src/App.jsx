5137b38b6d9170f98970c27d63cfd201f4c8cfaf
Yannick Hagedorn <176614986+y-hagedorn@users.noreply.github.com>
2024-09-09 13:45:59 +0200

    Exercise 2.8 phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,16 +2,30 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
     console.log('button clicked', event.target)
 
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
+    }
+
+    // Check if the name input is empty
+    if (newName.trim().length === 0) {
+      alert("Add a name to the number")
+      return
+    }
+
+    // Check if the number input is empty
+    if (newNumber.trim().length === 0) {
+      alert("Add a number to the name")
+      return
     }
 
     // Check if the name already exists in the persons array
@@ -20,6 +34,7 @@ const App = () => {
     } else {
       setPersons(persons.concat(personObject))
       setNewName('')
+      setNewNumber('')
     }
   }
 
@@ -28,23 +43,27 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleNumberInput = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+
+      <h2>Add a new Number</h2>
       <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handlePersonInput} />
-        </div>
-        <div>debug: {newName}</div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+        <div>name: <input name="nameEntry" value={newName} onChange={handlePersonInput} /></div>
+        <div>number: <input name="numberEntry" value={newNumber} onChange={handleNumberInput} /></div>
+        <div>debug: {newName}, {newNumber}</div>
+        <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
       <div>
         {persons.map(person =>
           <div key={person.name}>
-            {person.name}
+            {person.name} {person.number}
           </div>
         )}
       </div>
