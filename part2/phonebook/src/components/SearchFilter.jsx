94f22ef8b62e54e2ace67afe8ae40f2937d1d24e
Yannick Hagedorn <176614986+y-hagedorn@users.noreply.github.com>
2024-09-10 23:59:51 +0200

    Exercise 2.11 phonebook: initial state of data is fetched from  server using axios-library, fetching with an effect hook

--- part2/phonebook/src/components/SearchFilter.jsx
+++ part2/phonebook/src/components/SearchFilter.jsx
@@ -1,12 +1,7 @@
 const SearchFilter = ({ searchName, handleSearchInput }) => {
     return (
         <div>
-            Filter shown:
-            <input
-                name="searchInput"
-                value={searchName}
-                onChange={handleSearchInput}
-            />
+            Filter shown: <input name="searchInput" value={searchName} onChange={handleSearchInput}/>
         </div>
     )
 }
